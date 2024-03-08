469bbbb5799910ab54540ad6407683c0314d7180
P4t4m8n <eranopenuni@gmail.com>
2024-03-08 19:02:44 +0200

    Add demo users

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { UserModel } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  user = {
+  demoUserOne: UserModel = {
     _id: 'u101',
-    name: 'Bobovich',
-    username: 'Bobo',
-    isAdmin: true
+    userName: 'Bobo',
+    content: ['zv9xfmxysp', 'lluzsbzbizl', 'nh3p2c92h4s'],
+    following: ['u102'],
+    likes: ['7o6hayoj9ta']
+
+  }
+  demoUserTwo: UserModel = {
+    _id: 'u102',
+    userName: 'Vovo',
+    content: ['7o6hayoj9ta', 'ankzldcr1as', '600zdvs2p7h'],
+    following: [],
+    likes: []
+
   }
 
-  private _loggedInUser$ = new BehaviorSubject(this.user)
-  // private _loggedInUser$ = new BehaviorSubject(null)
+  private _loggedInUser$ = new BehaviorSubject(this.demoUserOne)
   public loggedInUser$ = this._loggedInUser$.asObservable()
 
   getLoggedInUser() {
